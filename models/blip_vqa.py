8e736fae25d1804603312491448bc9825de7a217
WAS <jordanslost@gmail.com>
2023-04-03 14:14:57 -0700

    Update blip_vqa.py

--- models/blip_vqa.py
+++ models/blip_vqa.py
@@ -7,9 +7,13 @@ import torch.nn.functional as F
 from transformers import BertTokenizer
 import numpy as np
 
+
+from pathlib import Path
+LOCAL_PATH = Path(__file__).parent
+
 class BLIP_VQA(nn.Module):
     def __init__(self,                 
-                 med_config = 'configs/med_config.json',  
+                 med_config = Path(LOCAL_PATH, 'configs/med_config.json'),  
                  image_size = 480,
                  vit = 'base',
                  vit_grad_ckpt = False,
