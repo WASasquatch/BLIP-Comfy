3cbb15f2d88fb2cb9c2c6b9e820c11cfb1ffc307
WAS <jordanslost@gmail.com>
2023-04-03 15:20:23 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -103,10 +103,10 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    #packages=find_packages(include=[NAME, f'{NAME}.*']),
+    packages=find_packages(include=[NAME, f'{NAME}.*']),
     #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=[NAME, f'{NAME}.configs', f'{NAME}.data', f'{NAME}.models', f'{NAME}.transform'],
+    #py_modules=[NAME, f'{NAME}.configs', f'{NAME}.data', f'{NAME}.models', f'{NAME}.transform'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
